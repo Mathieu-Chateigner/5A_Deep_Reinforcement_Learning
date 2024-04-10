34ad535deb7708a9204b592ca05b4550edebf463
Marc Brgs <marc.brgs@yahoo.fr>
2024-04-10 11:24:52 +0200

    Add monte carlo (buggy)

--- Deep Reinforcement Learning/Assets/Scripts/GameManager.cs
+++ Deep Reinforcement Learning/Assets/Scripts/GameManager.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Vector2Int gridSize;
+    public bool usePolicyIteration;
+    public bool useValueIteration;
+    public bool useMonteCarlo;
+
     public State start;
     public State end;
     public List<State> obstacles;
 
     private Policy policy;
-    private Dictionary<State, float> stateValues; // Used by PolicyEvaluation
+    private Dictionary<State, float> stateValues; // Used by PolicyIteration and ValueIteration
+
+    private Dictionary<State, float> returnsSum;
+    private new Dictionary<State, int> returnsCount; // Used by MonteCarlo
 
     void Start()
     {
@@ -24,13 +31,23 @@ public class GameManager : MonoBehaviour
 
         InitializeStateValues(states);
 
-        printCoordinates();
-
+        //printCoordinates();
         printPolicy();
 
-        PolicyIteration(states, 0.9f); // Policy iteration
-        //ValueIteration(states, 0.9f); // Value iteration
+        if(usePolicyIteration)
+        {
+            PolicyIteration(states, 0.9f); // Policy iteration
+        }
+        else if(useValueIteration)
+        {
+            ValueIteration(states, 0.9f); // Value iteration
+        }
+        else if(useMonteCarlo)
+        {
+            stateValues = MonteCarloFirstVisitOnPolicy(states, 20);
+        }
 
+        printPossibleActions();
         printStateValues();
         printPolicy();
     }
@@ -45,8 +62,6 @@ public class GameManager : MonoBehaviour
             new State(4, 2),
             new State(2, 3),
             new State(4, 3),
-            //new State(2, 2),
-            //new State(4, 0)
         };
     }
 
@@ -165,7 +180,7 @@ public class GameManager : MonoBehaviour
                 {
                     State nextState = GetNextState(state, action);
 
-                    float reward = IsEnd(nextState) ? 0f : GetImmediateReward(state, action); // To fix values between 0 and 1
+                    float reward = IsEnd(nextState) ? 0f : GetImmediateReward(state, action);
                     float value = reward + (discountFactor * stateValues[nextState]);
                     if (value > maxValue)
                     {
@@ -184,6 +199,148 @@ public class GameManager : MonoBehaviour
         } while (delta > theta);
     }
 
+    public Dictionary<State, float> MonteCarloFirstVisitOnPolicy(List<State> states, int numEpisode)
+    {
+        returnsSum = new Dictionary<State, float>();
+        returnsCount = new Dictionary<State, int>();
+
+        foreach (var state in states)
+        {
+            returnsSum[state] = 0.0f;
+            returnsCount[state] = 0;
+        }
+
+        for (int e = 0; e < numEpisode; e++)
+        {
+            // Generate using policy, an episode State0, Action0, Reward0, State1, Action1, Reward1, ..., StateT, ActionT, RewardT
+            // G = 0
+            // for timeStep t = T - 1 to t = 0 (of the episode e) do {
+            //      G  = G + RewardT+1
+            //      if StateT is not the sequence State0, State1, ..., StateT-1 then {
+            //          returnsSum[StateT] = returnsSum[StateT] + G
+            //          returnsCount[StateT] = returnsCount[StateT] + 1
+            //      }
+            // }
+
+            List<(State, Action, float)> episode = GenerateEpisode(); // Simulation
+
+            float G = 0;
+            HashSet<State> visitedStates = new HashSet<State>();
+
+            // Parcourir l'épisode de la fin au début
+            for (int t = episode.Count - 1; t >= 0; t--)
+            {
+                G = 0.9f * G + episode[t].Item3; // Récompense à t
+
+                State stateT = episode[t].Item1;
+
+                // First-Visit : si l'état n'a pas été déjà visité dans cet épisode
+                if (!visitedStates.Contains(stateT))
+                {
+                    returnsSum[stateT] += G;
+                    returnsCount[stateT] += 1;
+                    visitedStates.Add(stateT);
+                }
+            }
+
+            Dictionary<State, float> values = new Dictionary<State, float>();
+            foreach (State state in returnsSum.Keys)
+            {
+                if (IsEnd(state)) continue;
+                if (returnsCount[state] > 0)
+                {
+                    values[state] = returnsSum[state] / returnsCount[state];
+                }
+            }
+            UpdatePolicyBasedOnStateValues(states, values); // On policy
+        }
+
+        // Compute average
+        Dictionary<State, float> stateValues = new Dictionary<State, float>();
+        foreach (State state in returnsSum.Keys)
+        {
+            if (IsEnd(state)) continue;
+            if(returnsCount[state] > 0)
+            {
+                stateValues[state] = returnsSum[state] / returnsCount[state];
+            }
+        }
+        return stateValues;
+    }
+
+    private List<(State, Action, float)> GenerateEpisode()
+    {
+        List<(State, Action, float)> episode = new List<(State, Action, float)>();
+        State currentState = start; // Commencez à l'état de départ
+        HashSet<State> visitedStates = new HashSet<State>(); // Pour détecter les boucles
+
+        int step = 0;
+        while (!IsEnd(currentState))
+        {
+            Action action;
+            List<Action> validActions = GetValidActions(currentState);
+
+            if (Random.value < 0.9f) // Exploration avec une probabilité epsilon
+            {
+                action = validActions[Random.Range(0, validActions.Count)];
+            }
+            else // Exploitation
+            {
+                action = policy.GetAction(currentState);
+            }
+
+            //action = validActions[Random.Range(0, validActions.Count)];
+            //action = policy.GetAction(currentState); // Suit la politique
+            
+            State nextState = GetNextState(currentState, action);
+            float reward = GetImmediateReward(currentState, action);
+
+            // Boucle détectée ou état final impossible
+            if (step++ > 100)
+            {
+                episode.Add((currentState, default(Action), -1f)); // Echec
+                break;
+            }
+
+            episode.Add((currentState, action, reward));
+
+            currentState = nextState; // Mettez à jour l'état courant pour la prochaine itération
+            
+            if (IsEnd(currentState))
+            {
+                episode.Add((currentState, default(Action), 1f));
+                break;
+            }
+        }
+
+        return episode;
+    }
+
+    void UpdatePolicyBasedOnStateValues(List<State> states, Dictionary<State, float> values)
+    {
+        foreach (State state in states)
+        {
+            if (!IsEnd(state))
+            {
+                List<Action> validActions = GetValidActions(state);
+                float maxValue = float.NegativeInfinity;
+                Action bestAction = Action.Up;
+
+                foreach (Action action in validActions)
+                {
+                    State nextState = GetNextState(state, action);
+                    float value = values.ContainsKey(nextState) ? values[nextState] : 0f;
+                    if (value > maxValue)
+                    {
+                        maxValue = value;
+                        bestAction = action;
+                    }
+                }
+                policy.UpdatePolicy(state, bestAction);
+            }
+        }
+    }
+
     public float GetImmediateReward(State currentState, Action action)
     {
         State nextState = GetNextState(currentState, action);
@@ -193,7 +350,7 @@ public class GameManager : MonoBehaviour
         }
         else if (obstacles.Contains(nextState))
         {
-            return -1.0f;
+            return 0.0f;
         }
         else
         {
@@ -221,10 +378,10 @@ public class GameManager : MonoBehaviour
                 break;
         }
 
-        if (obstacles.Contains(nextState))
+        /*if (obstacles.Contains(nextState))
         {
             return state;
-        }
+        }*/
 
         return nextState;
     }
@@ -239,6 +396,22 @@ public class GameManager : MonoBehaviour
         if (state.X > 0) validActions.Add(Action.Left); // Peut aller vers la gauche
 
         // Check aussi les obstacles
+        if (obstacles.Contains(GetNextState(state, Action.Up)))
+        {
+            validActions.Remove(Action.Up);
+        }
+        if (obstacles.Contains(GetNextState(state, Action.Right)))
+        {
+            validActions.Remove(Action.Right);
+        }
+        if (obstacles.Contains(GetNextState(state, Action.Down)))
+        {
+            validActions.Remove(Action.Down);
+        }
+        if (obstacles.Contains(GetNextState(state, Action.Left)))
+        {
+            validActions.Remove(Action.Left);
+        }
 
         return validActions;
     }
@@ -312,5 +485,26 @@ public class GameManager : MonoBehaviour
             gridCoordinates += line + "\n";
         }
         Debug.Log(gridCoordinates);
+    }
+
+    private void printPossibleActions()
+    {
+        string grid = "Grid Possible Actions:\n";
+        for (int y = gridSize.y - 1; y >= 0; y--)
+        {
+            string line = "";
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                State state = new State(x, y);
+                int count = GetValidActions(state).Count;
+                if (obstacles.Contains(state))
+                {
+                    count = 0;
+                }
+                line += count + "\t";
+            }
+            grid += line + "\n";
+        }
+        Debug.Log(grid);
     }
 }
